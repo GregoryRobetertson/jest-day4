68f1410c15177fd055393716f5f752705bd57a1e
GregoryRobetertson <geefromla@gmail.com>
2024-04-26 15:00:39 -0500

    created

---
+++ components/WelcomeMessage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomeMessage = ({message}) => {
+    return (
+        <div>
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default WelcomeMessage;
---
+++ components/__tests__/WelcomeMessage.test.js
@@ -0,0 +1,10 @@
+import React, { experimental_useEffectEvent } from "react";
+import { render, getByText } from "@testing-library/react";
+import WelcomeMessage from "../WelcomeMessage";
+
+test('displays the message correctly', () => {
+    const message = 'Welcome to our website!';
+    const { getByText } = render(<WelcomeMessage message = {message}/>)
+    const messageElement = getByText(message);
+    expect(messageElement).toBeInTheDocument();
+})
