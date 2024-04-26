c16683404bbe76955297597a1f678903f43827d8
GregoryRobetertson <geefromla@gmail.com>
2024-04-26 15:57:53 -0500

    imported @testing-library/jest-dom

--- src/components/__tests__/WelcomeMessage.test.js
+++ src/components/__tests__/WelcomeMessage.test.js
@@ -1,7 +1,7 @@
 import React, { experimental_useEffectEvent } from "react";
 import { render, getByText } from "@testing-library/react";
 import WelcomeMessage from "../WelcomeMessage";
-
+import ' @testing-library/jest-dom';
 test('displays the message correctly', () => {
     const message = 'Welcome to our website!';
     const { getByText } = render(<WelcomeMessage message = {message}/>)
